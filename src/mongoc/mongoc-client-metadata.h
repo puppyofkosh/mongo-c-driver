8788afd5736be96cc485f944066f6c34538c2c97
ian boros <ian.boros@10gen.com>
2016-07-07 14:06:59 -0400

    More of hannes's comments

--- src/mongoc/mongoc-client-metadata.h
+++ src/mongoc/mongoc-client-metadata.h
@@ -7,6 +7,8 @@
 
 #include "mongoc-client.h"
 
+#define MONGOC_METADATA_APPLICATION_NAME_MAX 128
+
 bool mongoc_set_client_metadata (const char *driver_name,
                                  const char *driver_version,
                                  const char *platform);
