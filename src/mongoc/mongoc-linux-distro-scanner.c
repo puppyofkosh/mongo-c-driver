7761f8d791e30ea08eb099240ccb64b781d951a4
ian boros <ian.boros@10gen.com>
2016-07-22 12:06:56 -0400

    Added distro scanner

---
+++ src/mongoc/mongoc-linux-distro-scanner.c
@@ -0,0 +1,272 @@
+/*
+ * Copyright 2016 MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mongoc-error.h"
+#include "mongoc-log.h"
+#include "mongoc-version.h"
+#include "mongoc-util-private.h"
+
+static void
+_lsb_copy_val_if_need (const char  *val,
+                       char       **buffer)
+{
+   size_t sz;
+
+   if (*buffer) {
+      /* We've encountered this key more than once. This means the file is
+       * weird, so just keep first copy */
+      return;
+   }
+
+   sz = strlen (val) + 1;
+
+   /* Technically we could compute val_len, but these strings are tiny */
+   *buffer = bson_malloc (sz);
+   bson_strncpy (*buffer, val, sz);
+}
+
+static bool
+_lsb_process_line (char       **name,
+                   char       **version,
+                   const char  *line)
+{
+   size_t key_len;
+   const char *equal_sign;
+   const char *val;
+
+   const size_t line_len = strlen (line);
+
+   const char *delim = "=";
+   const size_t delim_len = strlen (delim);
+
+   /* Figure out where = is. Everything before is the key, and after is val */
+   equal_sign = strstr (line, delim);
+
+   if (equal_sign == NULL) {
+      /* This line is malformed/incomplete, so skip it */
+      return false;
+   }
+
+   /* Should never happen since we null terminated this line */
+   BSON_ASSERT (equal_sign < line + line_len);
+
+   key_len = equal_sign - line;
+   val = equal_sign + delim_len;
+
+   /* If we find two copies of either key, the *name == NULL check will fail
+    * so we will just keep the first value encountered. */
+   if (strncmp (line, "DISTRIB_ID", key_len) == 0) {
+      _lsb_copy_val_if_need (val, name);
+      return true;
+   } else if (strncmp (line, "DISTRIB_RELEASE", key_len) == 0) {
+      _lsb_copy_val_if_need (val, version);
+      return true;
+   }
+
+   return false;
+}
+
+
+bool
+_mongoc_linux_distro_scanner_parse_lsb (const char  *path,
+                                        char       **name,
+                                        char       **version)
+{
+   enum N { bufsize = 4096 };
+   char buffer [bufsize];
+   size_t buflen;
+
+   char *line;
+   char *line_end;
+
+   size_t cnt = 0;
+   FILE *f;
+
+   f = fopen (path, "r");
+
+   if (!f) {
+      return false;
+   }
+
+   /* Read first 4k bytes into buffer. Want to bound amount of time spent
+    * reading this file. If the file is super long, we may read an incomplete
+    * or unfinished line, but we're ok with that */
+   buflen = fread (buffer, sizeof (char), bufsize - 1, f);
+   buffer [buflen] = '\0';
+
+   while (cnt < buflen) {
+      line = buffer + cnt;
+
+      /* Find end of this line */
+      line_end = strstr (buffer + cnt, "\n");
+
+      if (line_end) {
+         *line_end = '\0';
+      } else {
+         line_end = &buffer[buflen];
+         BSON_ASSERT (*line_end == '\0');
+      }
+
+      cnt += (line_end - line + 1);
+
+      _lsb_process_line (name, version, line);
+
+      if (*version && *name) {
+         /* No point in reading any more */
+         break;
+      }
+   }
+
+   fclose (f);
+   return *version && *name;
+}
+
+/*
+ * Read whole first line or first 256 bytes, whichever is smaller
+ * -path is optional, only used for logging errors
+ * -It's your job to free the return value of this function
+ */
+static char *
+_read_first_line_up_to_limit (const char *path)
+{
+   enum N { bufsize = 256 };
+   char buffer[bufsize];
+   char *ret = NULL;
+   char *fgets_res;
+   size_t len;
+   FILE *f;
+
+   f = fopen (path, "r");
+
+   if (!f) {
+      MONGOC_WARNING ("Couldn't open %s: error %d", path, errno);
+      return NULL;
+   }
+
+   fgets_res = fgets (buffer, bufsize, f);
+
+   if (fgets_res) {
+      len = strlen (buffer);
+
+      if (buffer[len - 1] == '\n') {
+         /* get rid of newline */
+         buffer[len - 1] = '\0';
+         len--;
+      }
+
+      ret = bson_malloc (len + 1);
+      bson_strncpy (ret, buffer, len + 1);
+   } else if (ferror (f)) {
+      MONGOC_WARNING ("Could open but not read from %s, error: %d",
+                      path ? path : "<unkown>", errno);
+   } else {
+      /* The file is empty. Weird. Return null */
+   }
+
+   fclose (f);
+
+   return ret;
+}
+
+/*
+ * Find the first string in a list which is a valid file.
+ * Technically there's always a race condition when using this function
+ * since immediately after it returns, the file could get removed, so
+ * only use this for files which should never be removed (and check for
+ * NULL when you fopen again)
+ */
+static const char *
+_get_first_existing (const char **paths)
+{
+   const char **p = &paths[0];
+   FILE *f;
+
+   for (; *p != NULL; p++) {
+      f = fopen (*p, "r");
+
+      if (f) {
+         fclose (f);
+         return *p;
+      }
+   }
+
+   return NULL;
+}
+
+char *
+_mongoc_linux_distro_scanner_read_osrelease (const char *path)
+{
+   /* Read from something like /proc/sys/kernel/osrelease */
+   BSON_ASSERT (path);
+   return _read_first_line_up_to_limit (path);
+}
+
+char *
+_mongoc_linux_distro_scanner_read_release_file (const char *path)
+{
+   BSON_ASSERT (path);
+   return _read_first_line_up_to_limit (path);
+}
+
+static char *
+_find_and_read_release_file ()
+{
+   const char *path;
+   const char *paths [] = {
+      "/etc/system-release",
+      "/etc/redhat-release",
+      "/etc/novell-release",
+      "/etc/gentoo-release",
+      "/etc/SuSE-release",
+      "/etc/SUSE-release",
+      "/etc/sles-release",
+      "/etc/debian_release",
+      "/etc/slackware-version",
+      "/etc/centos-release",
+      "/etc/os-release",
+      NULL,
+   };
+
+   path = _get_first_existing (paths);
+
+   if (!path) {
+      return NULL;
+   }
+
+   return _mongoc_linux_distro_scanner_read_release_file (path);
+}
+
+bool
+_mongoc_linux_distro_scanner_get_distro (char **name,
+                                         char **version)
+{
+   const char *lsb_path = "/etc/lsb-release";
+   const char *osrelease_path = "/proc/sys/kernel/osrelease";
+
+   *name = NULL;
+   *version = NULL;
+
+   if (_mongoc_linux_distro_scanner_parse_lsb (lsb_path, name, version)) {
+      return true;
+   }
+
+   /* Otherwise get the name from the "release" file and version from
+    * /proc/sys/kernel/osrelease */
+   *name = _find_and_read_release_file ();
+   *version = _mongoc_linux_distro_scanner_read_osrelease (osrelease_path);
+
+   return (*name != NULL) && (*version != NULL);
+}
---
+++ tests/test-mongoc-linux-distro-scanner.c
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2016 MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <mongoc.h>
+#include "mongoc-client-private.h"
+#include "mongoc-linux-distro-scanner-private.h"
+
+#include "TestSuite.h"
+#include "test-libmongoc.h"
+#include "test-conveniences.h"
+
+
+static void
+test_parse_lsb ()
+{
+   char *name = NULL;
+   char *version = NULL;
+   bool ret;
+
+   ret = _mongoc_linux_distro_scanner_parse_lsb (
+      OS_RELEASE_FILE_DIR "/example-lsb-file.txt",
+      &name, &version);
+
+   ASSERT (ret);
+
+   ASSERT (name);
+   ASSERT (strcmp (name, "Ubuntu") == 0);
+
+   ASSERT (version);
+   ASSERT (strcmp (version, "12.04") == 0);
+
+   bson_free (name);
+   bson_free (version);
+}
+
+static void
+test_release_file ()
+{
+   char *name;
+   char *ver;
+   const char *release_path = OS_RELEASE_FILE_DIR "/example-release-file.txt";
+   const char *ver_path = OS_RELEASE_FILE_DIR "/example-os-version.txt";
+
+   name = _mongoc_linux_distro_scanner_read_release_file (release_path);
+
+   /* We expect to get the first line of the file (it's NOT parsing the file
+    * because we're not sure what format it is */
+   ASSERT (strcmp (name, "NAME=\"Ubuntu\"") == 0);
+
+   /* Normally would read from "/proc/sys/kernel/osrelease" */
+   ver = _mongoc_linux_distro_scanner_read_osrelease (ver_path);
+   ASSERT (strcmp (ver, "2.2.14-5.0") == 0);
+
+   bson_free (name);
+   bson_free (ver);
+}
+
+/* We only expect this function to actually read anything on linux platforms.
+ * We run this test on all platforms to be sure the get_distro function doesn't
+ * crash on a platform with some of the files it looks for missing */
+static void
+test_distro_scanner_reads ()
+{
+   char *name;
+   char *version;
+
+#ifndef __linux__
+   /* Want to ignore warnings that /proc/whatever doesn't exist.
+    * We still run the test since we want to be sure we don't crash! */
+   capture_logs (true);
+#endif
+   _mongoc_linux_distro_scanner_get_distro (&name, &version);
+
+   /*
+    * TODO: Remove this. Just for fun on the evergreen build
+    */
+   fprintf (stderr, "name: %s version: %s\n", name, version);
+   /* Remove it! */
+
+#ifdef __linux__
+   ASSERT (name);
+   ASSERT (strlen (name) > 0);
+   ASSERT (version);
+   ASSERT (strlen (version) > 0);
+#endif
+}
+
+void
+test_linux_distro_scanner_install (TestSuite *suite)
+{
+   TestSuite_Add (suite, "/LinuxDistroScanner/parse_lsb",
+                  test_parse_lsb);
+   TestSuite_Add (suite, "/LinuxDistroScanner/read_release_file",
+                  test_release_file);
+   TestSuite_Add (suite, "/LinuxDistroScanner/test_distro_scanner_reads",
+                  test_distro_scanner_reads);
+}
